f384101b0aa7942b08f4be83ba6c724e12cd73f4
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2022-09-15 13:47:06 +0700

    Hồng Đức - Update Contact

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// let slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//     let i;
-//     let slides = document.getElementsByClassName('slider');
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = 'none';
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) {
-//         slideIndex = 1;
-//     }
-//     slides[slideIndex - 1].style.display = 'block';
-//     setTimeout(showSlides, 10000); // Change image every 10 seconds
-
-//     let dots = document.getElementsByClassName('dot');
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(' active', '');
-//     }
-//     dots[slideIndex - 1].className += ' active';
-// }
 
 let slides = document.getElementsByClassName('slider');
 let dots = document.getElementsByClassName('dot');
